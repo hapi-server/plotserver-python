a5b95cb267986f206894c4f2e6796ef8b337b564
Robert Weigel <rweigel@gmu.edu>
2019-05-31 09:21:14 -0400

    Newline fix in version.py

--- misc/version.py
+++ misc/version.py
@@ -1,14 +1,19 @@
-import re
-import sys
+"""
+Reads CHANGES.txt and finds last version. Updates version strings in Makefile,
+setup.py, hapi.py, and hapiclient.py to match this version.
 
-# TODO: Find better way to manage this.
+TODO: Find better way to manage this.
+TODO: Update copyright year in LICENSE.txt
+"""
+
+import re
 
 # Get last version in CHANGES.txt
 print("Finding version information from CHANGES.txt")
 fin = open("CHANGES.txt")
 version = '0.0.0'
 for line in fin:
-	(repl,n) = re.subn(r"^v(.*):.*", r"\1", line)
+	(repl, n) = re.subn(r"^v(.*):.*", r"\1", line)
 	if n > 0:
 		version = repl
 fin.close()
@@ -24,8 +29,8 @@ for lineo in fin:
 fin.close()
 assert updated is False, "Problem updating version in Makefile."
 with open("Makefile.tmp", "w") as fout:
-    fout.write(lines + "\n")
-fout.close()    
+	fout.write(lines)
+fout.close()
 print("Wrote Makefile.tmp")
 
 lines = ''
@@ -37,36 +42,23 @@ for lineo in fin:
 fin.close()
 assert updated is False, "Problem updating version in setup.py."
 with open("setup.py.tmp", "w") as fout:
-    fout.write(lines + "\n")
-fout.close()    
+	fout.write(lines)
+fout.close()
 print("Wrote setup.py.tmp")
 
-for fname in ("hapiplotserver/hapiplotserver",):
+for fname in ("hapiclient/hapi.py", "hapiclient/hapiplot.py"):
 	lines = ''
 	fin = open(fname)
-	#sys.stdout.write("Updating version in " + fname + ".")
 	for lineo in fin:
-		line = re.sub(r"Version: (.*)", r"Version: " + version, lineo)
-		updated = lineo != line
-		lines = lines + line
+		line1 = re.sub(r"__version__ = '(.*)'", r"__version__ = '" + version + "'", lineo)
+		updated1 = lineo != line1
+		line2 = re.sub(r"Version: (.*)", r"Version: "+version, line1)
+		updated2 = lineo != line2
+		lines = lines + line2
 	fin.close()
-	assert updated is False, "Problem updating version in " + fname + "."
+	assert updated1 is False or updated2 is False, \
+		"Problem updating version in " + fname + "."
 	with open(fname + ".tmp", "w") as fout:
-	    fout.write(lines + "\n")
+		fout.write(lines)
 	fout.close()    
-	print("Wrote " + fname + ".tmp")
-
-for fname in ("hapiplotserver/main.py",):
-	lines = ''
-	fin = open(fname)
-	#sys.stdout.write("Updating version in " + fname + ".")
-	for lineo in fin:
-		line = re.sub(r'__version__ = "(.*)"', r'__version__ = "' + version + '"', lineo)
-		updated = lineo != line
-		lines = lines + line
-	fin.close()
-	assert updated is False, "Problem updating version in " + fname + "."
-	with open(fname + ".tmp", "w") as fout:
-	    fout.write(lines + "\n")
-	fout.close()
 	print("Wrote " + fname + ".tmp")
