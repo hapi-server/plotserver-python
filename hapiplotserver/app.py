c758486f683e9a2bbae44feb80f4787821fd0b98
Robert Weigel <rweigel@gmu.edu>
2019-06-04 09:40:25 -0400

    gallery improvements

--- hapiplotserver/app.py
+++ hapiplotserver/app.py
@@ -2,11 +2,11 @@ import os
 
 from hapiclient import hapi
 from hapiplotserver.plot import plot
-from hapiplotserver.viviz import prepviviz
+from hapiplotserver.viviz import prepviviz, req2slug
 
 def app(conf):
 
-    from flask import Flask, request, redirect, send_from_directory, url_for
+    from flask import Flask, request, redirect, send_from_directory, make_response, url_for
     application = Flask(__name__)
 
     loglevel = conf['loglevel']
@@ -14,7 +14,7 @@ def app(conf):
     server_usecache = conf['usecache']
     appdir = os.path.abspath(os.path.dirname(__file__))
 
-    @application.route("/html/favicon.ico")
+    @application.route("/favicon.ico")
     def favicon():
         if request.args.get('server') is None:
             return send_from_directory(appdir + "/html/", "favicon.ico")
@@ -42,8 +42,8 @@ def app(conf):
             return 'A server argument is required, e.g., /?server=...', 400, {'Content-Type': 'text/html'}
 
         dataset = request.args.get('id')
-        if dataset is None:
-            return 'A dataset argument is required, e.g., /?server=...&id=...', 400, {'Content-Type': 'text/html'}
+        if dataset is None and format != 'gallery':
+            return 'A dataset id argument is required, e.g., /?server=...&id=...', 400, {'Content-Type': 'text/html'}
 
         parameters = request.args.get('parameters')
         if parameters is None and format != 'gallery':
@@ -58,14 +58,14 @@ def app(conf):
             return 'A time.max argument is required if format != "gallery", e.g., /?server=...&id=...&parameters=...', 400, {'Content-Type': 'text/html'}
 
         meta = None
-        if start is None:
+        if start is None and format != 'gallery':
             try:
                 meta = hapi(server, dataset)
                 start = meta['startDate']
             except Exception as e:
                 return 'Could not get metadata from ' + server, 400, {'Content-Type': 'text/html'}
 
-        if stop is None:
+        if stop is None and format != 'gallery':
             if meta is None:
                 try:
                     meta = hapi(server, dataset)
@@ -86,7 +86,7 @@ def app(conf):
         if server_usecache is False and usecache is True:
             usecache = False
             if loglevel == 'debug':
-                print('Application configuration has usecache=False so request to use cache is ignored.')
+                print('app(): Application configuration has usecache=False so request to use cache is ignored.')
 
         transparent = request.args.get('transparent')
         if transparent is None:
@@ -114,20 +114,13 @@ def app(conf):
             figsize = (float(figsizearr[0]), float(figsizearr[1]))
             # TODO: Set limits on figsize?
 
-        id = ""
-        if parameters is not None:
-            id = "&id=" + parameters.split(",")[0]
-        else:
-            meta = hapi(server, dataset)
-            # Set first parameter shown to be first in dataset (if this is
-            # not done the time variable is the first parameter shown, which
-            # is generally not wanted.)
-            id = "&id=" + meta['parameters'][1]['name']
-
         if format == 'gallery':
-            prepviviz(server, dataset, **conf)
+            indexhtm, vivizhash = prepviviz(server, dataset, parameters, start, stop, **conf)
+            # Get full URL
             url = url_for("viviz", _external=True)
-            return redirect(url+"#catalog="+server + id, code=302)
+            red = url + indexhtm + "#" + vivizhash
+            print("Redirecting to " + red)
+            return redirect(red, code=302)
 
         # Plot options
         opts = {'cachedir': cachedir, 'usecache': usecache, 'loglevel': loglevel, 'format': format, 'figsize': figsize, 'dpi': dpi, 'transparent': transparent}
@@ -141,24 +134,23 @@ def app(conf):
 
         return img[0], 200, ct
 
-    # ../? in fuldir and thumbdir in viviz.py
-    # return = url + "#catalog=..." and url = url_for("viviz", _external=True)
-    # in format == 'gallery'
-
     # Serve index.htm for /viviz/ request
     @application.route("/viviz/")
     def viviz():
-        return send_from_directory(cachedir+"/viviz", "index.htm")
+        pass
 
     # Serve static files
     @application.route("/viviz/"+"<path:filename>")
     def vivizf(filename):
-        print(filename)
-        if filename == 'index.js' or '.json' in filename:
-            # Force these files to not be cached by browser.
-            print("Setting cache_timeout = 0 for " + filename)
-            return send_from_directory(cachedir+"/viviz", filename, cache_timeout=0)
-        else:
-            return send_from_directory(cachedir+"/viviz", filename)
+        fname, fext = os.path.splitext(cachedir+"/viviz/" + filename)
+        response = make_response(send_from_directory(cachedir+"/viviz", filename))
+        if not fext:
+            response.headers['Content-Type'] = 'text/html'
+            response.headers['Content-Disposition'] = 'inline'
+        return response
+
+    @application.errorhandler(500)
+    def internal_error(error):
+        print(error)
 
     return application
--- hapiplotserver/viviz.py
+++ hapiplotserver/viviz.py
@@ -1,4 +1,4 @@
-def prepviviz(server, dataset, **kwargs):
+def prepviviz(server, dataset, parameters, start, stop, **kwargs):
     """Prepare ViViz web application"""
 
     import os
@@ -12,11 +12,14 @@ def prepviviz(server, dataset, **kwargs):
         if kwargs['loglevel'] == 'debug':
             print('hapiplotserver.viviz(): Found ViViz at ' + vivizdir)
 
-    vivizconfig(server, dataset, **kwargs)
+    return vivizconfig(server, dataset, parameters, start, stop, **kwargs)
+
 
 def getviviz(**kwargs):
     """Download ViViz web application"""
 
+    # TODO: Copy index.js to index-orig.js
+    # In vivizconfig() copy index-orig.js to filename associated with request URL
     import shutil
     import zipfile
     from hapiclient.util import system, download
@@ -24,38 +27,141 @@ def getviviz(**kwargs):
     url = 'https://github.com/rweigel/viviz/archive/master.zip'
     file = kwargs['cachedir'] + '/viviz-master.zip'
     if shutil.which('git'):
-        code, stderr, stdout = system('git clone https://github.com/rweigel/viviz.git ' + kwargs['cachedir'] + '/viviz')
+        print('hapiplotserver.getviviz(): Cloning ViViz ' + kwargs['cachedir'])
+        code, stderr, stdout = system('git clone --depth 1 https://github.com/rweigel/viviz.git ' + kwargs['cachedir'] + '/viviz')
         if kwargs['loglevel'] == 'debug':
-            print(code, stderr, stdout)
+            if code != 0:
+                print('hapiplotserver.getviviz(): stdout:\n' + stdout)
+                print('hapiplotserver.getviviz(): stderr:\n' + stderr)
     else:
+        print('hapiplotserver.getviviz(): Downloading ' + url)
         download(file, url)
         zipref = zipfile.ZipFile(file, 'r')
         zipref.extractall(kwargs['cachedir'])
         zipref.close()
 
 
-def vivizconfig(server, dataset, **kwargs):
-    """Create ViViz configuration catalog"""
+def req2slug(server, dataset, parameters, start, stop):
+    """Convert HAPI request parameters to santized string for part of file name"""
+
+    from slugify import slugify
+
+    # str() is used to convert None to 'None' if needed.
+    slug = server + '-' + str(dataset) + '-' + str(parameters) + '-' + str(start) + '-' + str(stop)
+    slug = slugify(slug, replacements=[['|', '_or_'], ['%', '_pct_']])
+
+    return slug
+
+
+def vivizconfig(server, dataset, parameters, start, stop, **kwargs):
+    """Create ViViz configuration catalog and return ViViz URL hash"""
+
+    import os
+    import shutil
+    import hashlib
+    from hapiclient import hapi
+
+    slug = req2slug(server, dataset, parameters, start, stop)
+
+    indexjs_rel = 'viviz/index-' + slug + '.js'
+    indexjs = kwargs['cachedir'] + '/' + indexjs_rel
+    indexjso = kwargs['cachedir'] + '/viviz/index.js'
+    shutil.copyfile(indexjso, indexjs)
+
+    indexhtm = kwargs['cachedir'] + '/viviz/index-' + slug + '.htm'
+    indexhtmo = kwargs['cachedir'] + '/viviz/index.htm'
+    shutil.copyfile(indexhtmo, indexhtm)
+
+    fid = hashlib.md5(bytes(slug, 'utf8')).hexdigest()
+    indexhtm_hash = fid[0:4]
+    if not os.path.isfile(indexhtm_hash):
+        os.symlink(indexhtm, indexhtm_hash)
+
+    indexjs_rel = 'index-' + slug + '.js'
+    with open(indexhtm) as f:
+        tmp = f.read().replace('<script type="text/javascript" src="index.js',
+                               '<script type="text/javascript" src="' + indexjs_rel)
+    with open(indexhtm, "w") as f:
+        f.write(tmp)
+
+    if dataset is None:
+        catalog = hapi(server)
+        dataset0 = catalog["catalog"][0]["id"]  # First dataset
+        for i in range(len(catalog["catalog"])):
+            dataset = catalog["catalog"][i]["id"]
+            adddataset(server, dataset, indexjs, **kwargs)
+    else:
+        dataset0 = dataset
+        adddataset(server, dataset, indexjs, **kwargs)
+
+    gid = ""
+    # ID is ViViz gallery ID. In ViViz, the server URL is the catalog ID.
+    if parameters is not None:
+        gid = "&id=" + parameters.split(",")[0]
+    else:
+        if dataset is not None:
+            meta = hapi(server, dataset0)
+            # Set first parameter shown to be first in dataset (if this is
+            # not done the time variable is the first parameter shown, which
+            # is generally not wanted.)
+            gid = "&id=" + meta['parameters'][1]['name']
+
+    vivizhash = "catalog=" + server + "/" + dataset0 + gid
+
+    return indexhtm_hash, vivizhash
+
+
+def adddataset(server, dataset, indexjs, **kwargs):
 
     import os
     import json
-    import shutil
-    from hapiclient.hapi import hapi, server2dirname
+    from hapiclient.hapi import hapi, hapitime2datetime, server2dirname
 
-    indexjs = kwargs['cachedir'] + '/viviz/index.js'
-
-    fname = server2dirname(server) + '_' + dataset + '.json'
+    fname = server2dirname(server) + '/' + dataset + '.json'
     catalogabs = kwargs['cachedir'] + '/viviz/catalogs/' + fname
     catalogrel = 'catalogs/' + fname
 
+    dname = os.path.dirname(catalogabs)
+    if not os.path.exists(dname):
+        os.makedirs(dname)
+
+    if kwargs['loglevel'] == 'debug':
+        print('hapiplotserver.catalog(): Appending to ' + indexjs)
+
+    with open(indexjs, 'a') as f:
+        f.write('\nVIVIZ["config"]["catalogs"]["%s/%s"] = {};\n' % (server, dataset))
+
+    with open(indexjs, 'a') as f:
+        f.write('VIVIZ["config"]["catalogs"]["%s/%s"]["URL"] = "%s";\n' % (server, dataset, catalogrel))
+
+    if False and os.path.exists(catalogabs):
+        if kwargs['loglevel'] == 'debug':
+            print('hapiplotserver.catalog(): Using cached ' + catalogabs)
+        return
+
     meta = hapi(server, dataset)
 
+    def adjust_time(hapitime, ndays):
+        """Convert HAPI Time to %Y-%m-%d and add +/1 day"""
+
+        from datetime import datetime, date, timedelta
+        from hapiclient import hapitime2datetime
+
+        dt = hapitime2datetime(hapitime)[0]
+        if dt.strftime('%Y-%m-%dT%H:%M:%D.%f') != dt.strftime('%Y-%m-%dT00:00:00.000000'):
+            dt = datetime.date(dt) + timedelta(days=ndays)
+
+        # The following is needed as it will convert startDate in YYYY-DOY
+        # format to YYYY-MM-DD, which ViViz only handles.
+        time_string = dt.strftime('%Y-%m-%d')
+        return time_string
+
     gallery = {
-                'id': server,
+                 'id': server,
                  'aboutlink': server,
                  'strftime': "time.min=$Y-$m-$dT00:00:00.000Z&time.max=$Y-$m-$dT23:59:59.999Z",
-                 'start': meta['startDate'],
-                 'stop': meta['stopDate'],
+                 'start': adjust_time(meta['startDate'], 1),
+                 'stop': adjust_time(meta['stopDate'], -1),
                  'fulldir': ''
                 }
 
@@ -71,27 +177,8 @@ def vivizconfig(server, dataset, **kwargs):
         galleryc['aboutlink'] = server + "/info?id=" + dataset
         galleries.append(galleryc)
 
-    indexjs_original = indexjs.replace(".js","-original.js")
-    if not os.path.isfile(indexjs_original):
-        shutil.copyfile(indexjs, indexjs_original)
-    else:
-        shutil.copyfile(indexjs_original, indexjs)
-
-    dname = os.path.dirname(catalogabs)
-    if not os.path.exists(dname):
-        os.makedirs(dname)
-
     if kwargs['loglevel'] == 'debug':
         print('hapiplotserver.catalog(): Writing ' + catalogabs)
 
     with open(catalogabs, 'w') as f:
         json.dump(galleries, f, indent=4)
-
-    if kwargs['loglevel'] == 'debug':
-        print('hapiplotserver.catalog(): Appending to ' + indexjs)
-
-    with open(indexjs,'a') as f:
-        f.write('\nVIVIZ["config"]["catalogs"]["%s"] = {};\n' % server)
-
-    with open(indexjs,'a') as f:
-        f.write('VIVIZ["config"]["catalogs"]["%s"]["URL"] = "%s";\n' % (server, catalogrel))
