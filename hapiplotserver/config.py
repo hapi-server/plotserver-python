8bd4008bbf0d72b4b4efdfef71795688df820b8f
Robert Weigel <rweigel@gmu.edu>
2019-06-10 18:51:47 -0400

    refactor config.py

--- hapiplotserver/config.py
+++ hapiplotserver/config.py
@@ -4,31 +4,36 @@ def config(**kwargs):
     import logging
     import tempfile
 
-    # If > 0, use Gunicorn with this many workers. THREADED is ignored.
-    WORKERS = 0
+    # If `workers` > 0, use Gunicorn with this many workers;
+    # `threaded` is ignored.
+    workers = 0
 
-    LOGLEVEL = 'default'
+    loglevel = 'default'
     # error: show only errors
     # default: show requests and errors
     # debug: show requests, errors, and debug messages
 
-    conf = {}
-    conf['port'] = kwargs['port'] if 'port' in kwargs else 5000
-    conf['cachedir'] = kwargs['cachedir'] if 'cachedir' in kwargs else os.path.join(tempfile.gettempdir(), 'hapi-data')
-    conf['usecache'] = kwargs['usecache'] if 'usecache' in kwargs else True
-    conf['usedatacache'] = kwargs['usedatacache'] if 'usedatacache' in kwargs else True
-    conf['loglevel'] = kwargs['loglevel'] if 'loglevel' in kwargs else LOGLEVEL
-    conf['threaded'] = kwargs['threaded'] if 'threaded' in kwargs else True
-    conf['workers'] = kwargs['workers'] if 'workers' in kwargs else WORKERS
-    conf['figsize'] = kwargs['figsize'] if 'figsize' in kwargs else (7, 3)
-    conf['format'] = kwargs['format'] if 'format' in kwargs else 'png'
-    conf['dpi'] = kwargs['dpi'] if 'dpi' in kwargs else 144
-    conf['transparent'] = kwargs['transparent'] if 'transparent' in kwargs else False
+    conf = {"port": 5000,
+            "cachedir": os.path.join(tempfile.gettempdir(), 'hapi-data'),
+            "usecache": True,
+            "usedatacache": True,
+            "loglevel": loglevel,
+            "threaded": True,
+            "workers": workers,
+            "figsize": (7, 3),
+            "format": 'png',
+            "dpi": '144',
+            "transparent": False}
+
+    for key in conf:
+        if key in kwargs:
+            conf[key] = kwargs[key]
 
     # TODO: Add log-to-file option.
     # https://gist.github.com/ivanlmj/dbf29670761cbaed4c5c787d9c9c006b
     if conf['loglevel'] == 'error':
         log = logging.getLogger('werkzeug')
         log.setLevel(logging.ERROR)
+
     return conf
 
