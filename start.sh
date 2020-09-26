ec0ce635a15e134c06694a8f9ec3e0f3c8ade2d5
Bob Weigel <rweigel@gmu.edu>
2020-09-26 10:19:06 -0400

    Add timeout to prevent worker reboot

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source ~/.bashrc
-conda activate python3.6;
-python setup.py develop;
-hapiplotserver --workers 4 --port 5000
+conda activate python3.6
+python setup.py develop
+nohup hapiplotserver --workers 3 --port 5000 2>&1 &
+sleep 2
+tail -20 nohup.out &
