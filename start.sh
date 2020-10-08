d6a4ca2e0740ce22e878e357db36de0d163d6d42
Bob Weigel <rweigel@gmu.edu>
2020-10-08 15:37:26 -0400

    upstart script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 source ~/.bashrc
 conda activate python3.6
 python setup.py develop
-nohup hapiplotserver --workers 3 --port 5000 2>&1 &
-sleep 2
-tail -20 nohup.out &
+hapiplotserver --workers 3 --port 5000 2>&1 &
+#nohup hapiplotserver --workers 3 --port 5000 2>&1 &
+#sleep 2
+#tail -20 nohup.out &
