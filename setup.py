c8a2f3f0968ab09318fad51bf8ea6679a000337f
Bob Weigel <rweigel@gmu.edu>
2022-03-01 10:42:06 -0500

    Last 0.1.1 commit

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'develop':
 # version is modified by misc/version.py. See Makefile.
 setup(
     name='hapiplotserver',
-    version='0.1.0',
+    version='0.1.1',
     author='Bob Weigel',
     author_email='rweigel@gmu.edu',
     packages=find_packages(),
