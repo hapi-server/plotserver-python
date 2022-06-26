492fee75a8f53c62ed96284349a3141e21bcb349
Bob Weigel <rweigel@gmu.edu>
2022-06-26 19:30:37 -0400

    launch daemons

--- etc/hapiplotserver.sh
+++ etc/hapiplotserver.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 source anaconda3/etc/profile.d/conda.sh; conda activate
-conda activate python3.6
+#conda activate python3.6
+conda activate python3.7
 python setup.py develop
-hapiplotserver --workers 3 --port 5000 2>&1 &
+#hapiplotserver --workers 2 --port 5000 2>&1 &
 
 # When proxied to internal network
-#hapiplotserver --workers 3 --port 5000 --bind 0.0.0.0 2>&1 &
+hapiplotserver --workers 3 --port 5000 --bind 0.0.0.0 2>&1 &
 
 #nohup hapiplotserver --workers 3 --port 5000 2>&1 &
 #sleep 2
