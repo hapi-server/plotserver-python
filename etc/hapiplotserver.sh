8a71f75596cfc1fe13864c74fb3e8a52b53c88a6
Bob Weigel <rweigel@gmu.edu>
2023-01-09 12:26:16 -0500

    pass cachdir

--- etc/hapiplotserver.sh
+++ etc/hapiplotserver.sh
@@ -9,4 +9,5 @@ source anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate python3.7
 
 # When proxied to internal network
-hapiplotserver --workers 5 --port 5000 --bind 0.0.0.0 #2>&1 &
+#hapiplotserver --workers 5 --port 5000 --bind 0.0.0.0 --cachedir cache --loglevel debug #2>&1 &
+hapiplotserver --workers 5 --port 5000 --bind 0.0.0.0 --cachedir cache #2>&1 &
